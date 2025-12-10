510602cc7dc9db0a650303246eb6616433abfc45
MateoMaibach <mateo1maibach@gmail.com>
2025-12-10 20:42:12 -0300

    modularización index

--- src/index.js
+++ src/index.js
@@ -44,3 +44,9 @@ app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
   console.log(`Swagger: http://localhost:${PORT}/api/docs`);
 });
+
+
+/*lorem i"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna 
+aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."*/
